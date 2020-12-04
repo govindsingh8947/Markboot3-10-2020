60dcedd194ff9756323dd81dbd56d597ca2cd5f5
govindsingh8947 <69807083+govindsingh8947@users.noreply.github.com>
2020-12-04 13:38:46 +0530

    Add files via upload

---
+++ Markboot 4-12-2020/lib/common/commonFunc.dart
@@ -0,0 +1,96 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class CommonFunction {
+
+  final String serverToken = 'AAAALKQaw9U:APA91bHOFcWpFCw1mSZq7bRhlSkQBdKXg_OhV4M1__RrGwzx7uZubmOD37nZdnmZKzfbSNDMwDFGVbAhTpsYpu0lePZkuwjW2SgOUhNb57Cwd0YPI6cbjSXIKvojYFpNirTuB1L3rVkT';
+  final FirebaseMessaging firebaseMessaging = FirebaseMessaging();
+
+  Firestore _firestore = Firestore();
+
+  Future<void> showProgressDialog({@required bool isShowDialog,@required context}) {
+    if(isShowDialog) {
+      showDialog(context: context,
+          builder: (context){
+            return Center(
+              child: Container(
+                width: 30,
+                height: 30,
+                child: CircularProgressIndicator(),
+              ),
+            );
+          });
+    }
+    else if(Navigator.canPop(context)) {
+      Navigator.pop(context);
+    }
+  }
+
+  Future<List<DocumentSnapshot>> getPost(String path)async {
+    try {
+      List<DocumentSnapshot> documents = new List<DocumentSnapshot>();
+//      debugPrint("SnapshotNAME 123");
+//      print(path);
+      QuerySnapshot querySnapshot =  await _firestore.collection(path).getDocuments();
+      //print(querySnapshot);
+      for(DocumentSnapshot snapshot in querySnapshot.documents) {
+        //print(snapshot.data);
+        //debugPrint("SnapshotNAME ${snapshot["task"]}");
+        documents.add(snapshot);
+      }
+      return documents;
+    }
+    catch(e) {
+      //debugPrint("Exception (getRecentPost) : ${e.toString()}");
+    }
+  }
+
+  Future<Map<String, dynamic>> sendAndRetrieveMessage(String body,String title) async {
+    await firebaseMessaging.requestNotificationPermissions(
+      const IosNotificationSettings(sound: true, badge: true, alert: true, provisional: false),
+    );
+    var jsonRes = await http.post(
+      'https://fcm.googleapis.com/fcm/send',
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+        'Authorization': 'key=$serverToken',
+      },
+      body: json.encode(
+        <String, dynamic>{
+          'notification': <String, dynamic>{
+            'body':body,
+            'title':title
+          },
+          'priority': 'high',
+          'data': <String, dynamic>{
+            'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+            'id': '1',
+            'status': 'done'
+          },
+          'to': "/topics/all",
+        },
+      ),
+    );
+
+    var jsonData = jsonRes.body;
+    debugPrint("msgReqData $jsonData");
+
+    final Completer<Map<String, dynamic>> completer =
+    Completer<Map<String, dynamic>>();
+
+    firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        completer.complete(message);
+      },
+    );
+
+    return completer.future;
+  }
+
+
+}
---
+++ Markboot 4-12-2020/lib/common/style.dart
@@ -0,0 +1,25 @@
+class CommonStyle {
+//  int blueColor = 0xff162954;
+  int blueColor = 0xff00ccff;
+//  int blueCardColor = 0xff294073;
+  int blueCardColor = 0xffDCDCDC;
+  int darkBlueColor = 0xffDCDCDC;
+//  int darkBlueColor = 0xffe5e7e9  ;
+  int backgroundColor = 0xffe5e7e9;
+//  int appbarColor = 0xff3498db;
+  int appbarColor = 0xff00ccff;
+  int greyColor = 0xfff5f5f5;
+  int lightYellowColor = 0xffF3BA1D;
+  int inputFieldFillColor = 0xff48556E;
+  int lightGreen = 0xff34E233;
+//  int cardColor2 = 0xff4f2e15;
+  int cardColor2 = 0xffA9A9A9;
+  int lightCoffieColor = 0xffBA8E37;
+  int cardColor1 = 0xffA9A9A9;
+//  int cardColor1 = 0xff0D294E;
+  int cardColor4 = 0xffA9A9A9;
+//  int cardColor4 = 0xff2a3327;
+  int cardColor3 = 0xffA9A9A9;
+//  int cardColor3 = 0xff291542;
+  int introBackgroundColor = 0xff2a2a28;
+}
